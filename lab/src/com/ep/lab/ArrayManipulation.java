4328aecbb457dbbf9167925bcc4ecc2edaaf4774
hscavino <hugo@scavino.org>
2025-02-07 16:49:44 -0500

    Initial lab

---
+++ lab/src/com/ep/lab/ArrayManipulation.java
@@ -0,0 +1,45 @@
+package com.ep.lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayManipulation {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+            // Input array size
+        System.out.println("Enter the number of elements in the array");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+
+        // input array elements
+        System.out.println("Enter " + size + " integers");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        // TODO: Calculate the sum of the array elements
+
+        // TODO: Calculate the average of the array elements
+
+        // TODO: Find the maximum value in the array
+
+        // TODO: Find the minimum value in the array
+
+        // TODO: Search for a specific value in the array
+
+        // TODO Sort the array in ascending order
+
+        // TODO: Reverse the array
+
+        // Display Results
+
+        System.out.println("Original array: " + Arrays.toString(numbers));
+
+        // TODO: Display the sum, average, max, min, sorted array and reversed
+
+        // Close the scanner
+        scanner.close();
+    }
+}
